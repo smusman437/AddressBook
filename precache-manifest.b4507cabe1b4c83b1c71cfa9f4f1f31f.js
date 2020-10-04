7e61003398e25e4a88a453664c9e551e9c7ca6dc
smusman437 <smusman437@gmail.com>
2020-10-04 09:13:49 +0500

    Updates

---
+++ precache-manifest.b4507cabe1b4c83b1c71cfa9f4f1f31f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0ea17391f2dee272ed5fc39c336a740d",
+    "url": "/AddressBook/index.html"
+  },
+  {
+    "revision": "e3d75b6afa39d839f6d6",
+    "url": "/AddressBook/static/css/main.8e336c24.chunk.css"
+  },
+  {
+    "revision": "d4917c82ad41fbd740cc",
+    "url": "/AddressBook/static/js/2.1ac29b2e.chunk.js"
+  },
+  {
+    "revision": "4acd90554931a5346c73c3af099efcb8",
+    "url": "/AddressBook/static/js/2.1ac29b2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3d75b6afa39d839f6d6",
+    "url": "/AddressBook/static/js/main.07e23a36.chunk.js"
+  },
+  {
+    "revision": "c537f1853a339bc7237a",
+    "url": "/AddressBook/static/js/runtime-main.7c28d97e.js"
+  }
+]);
